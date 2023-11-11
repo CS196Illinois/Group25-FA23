e12d0fba360c7bcb20bebd4aa57e3059317c2a0a
Velizar Zlatev <vzlatevbul@gmail.com>
2023-11-10 19:57:38 -0600

    working prototype, problems with the Model (field, organizer) and styling

--- Project/backend/event/urls.py
+++ Project/backend/event/urls.py
@@ -2,5 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.getEvents),
+    path('', views.events, name='events'),
+    path('events/<int:pk>/', views.event_detail, name='event_detail')
 ]
