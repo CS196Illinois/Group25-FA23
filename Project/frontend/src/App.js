f16dcf4fa36339fecfab1d9403f6de1dc3255ea8
Velizar Zlatev <vzlatevbul@gmail.com>
2023-11-12 19:27:12 -0600

    everything is here

--- Project/frontend/src/App.js
+++ Project/frontend/src/App.js
@@ -1,16 +1,18 @@
-import logo from './logo.svg';
-import './App.css';
-import ShowEvents from './components/ShowEvents';
-import NavBar from './components/NavBar';
-import {BrowserRouter as Router, Route, Routes} from 'react-router-dom'
+import logo from "./logo.svg";
+import "./App.css";
+import ShowEvents from "./components/ShowEvents";
+import NavBar from "./components/NavBar";
+import CreateEvent from "./components/CreateEvent";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 
 function App() {
   return (
-    <div className="App">
+    <div className='App'>
       <Router>
         <NavBar />
         <Routes>
-          <Route path="/" element={<ShowEvents />} />
+          <Route path='/' element={<ShowEvents />} />
+          <Route path='/create-event/' element={<CreateEvent />} />
         </Routes>
       </Router>
     </div>
