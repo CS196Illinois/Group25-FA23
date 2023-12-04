ab954ef75af109562c608f9d1226a9a189271d10
irxebi <103395941+irxebi@users.noreply.github.com>
2023-12-04 14:50:08 -0600

    login page

--- Project/frontend/src/pages/LoginPage.js
+++ Project/frontend/src/pages/LoginPage.js
@@ -1,29 +1,21 @@
 import React, {useContext} from 'react'
+import { Form, Image, Button } from 'react-bootstrap';
 import AuthContext from '../context/AuthContext'
 
 const LoginPage = () => {
     let {loginUser} = useContext(AuthContext)
     return (
-        <div className = 'outer loginPage'>
-        <section id = "section">
-        <center>
-        <h1 id = "logoImg"> <img src="https://i.imgur.com/t0DWiN9.png" alt="Your Logo" /></h1>
-        </center>
-<form onSubmit={loginUser}>
-    
-<br></br>
-<br></br>
-<br></br>
-<br></br>
-
-<label className ="title" for="user">USERNAME</label> 
-            <input type="user" name="username"/>
-            <label className = 'title'for="pass">PASSWORD</label> 
-            <input type="pass" name="password"/>
-            <button type="submit">SIGN IN</button>
-           
-            </form>
-        </section>
+        <div className='outer loginPage'>
+            <section id="section">
+                <Image src="https://i.imgur.com/t0DWiN9.png" width="auto" height="auto" fluid rounded></Image>
+                <Form onSubmit={loginUser}>
+                    <Form.Label className ="title" for="user">USERNAME</Form.Label> 
+                    <Form.Control type="user" name="username"/>
+                    <Form.Label className = 'title'for="pass">PASSWORD</Form.Label> 
+                    <Form.Control type="pass" name="password"/>
+                    <Button type="submit">SIGN IN</Button>
+                </Form>
+            </section>
         </div>
         
     )
